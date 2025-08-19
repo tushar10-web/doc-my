b940eac9c828ad05efcbca3787e2c158be5425a5
tushar10-web <tusharr.8487@gmail.com>
2025-08-19 13:07:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -23,7 +23,8 @@ function App() {
     formData.append('image', file);
 
     try {
-      const response = await fetch('http://localhost:5000/upload', {
+     const response = await fetch('https://doc-my-1.onrender.com/upload', {
+
         method: 'POST',
         body: formData,
       });
